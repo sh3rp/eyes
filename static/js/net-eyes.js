5db9a7f5d8c72dcc85859b16b2b3fc67dd3e9f77
Shep Kendall <s@kndl.org>
2017-07-19 16:59:52 -0400

    beginning the ui

---
+++ static/js/net-eyes.js
@@ -0,0 +1,26 @@
+
+function addActiveSwitcher() {
+    $('.navbar-nav li').click(function(e) {
+        $('.navbar li.active').removeClass('active');
+        var $this = $(this);
+        if (!$this.hasClass('active')) {
+            $this.addClass('active');
+        }
+        e.preventDefault();
+        switch(e.target.id) {
+            case "agents": // load agents
+                alert("Loading agents.");
+                break;
+            case "schedules": // load schedules
+                alert("Loading schedules.");
+                break;
+            case "visuals": // load visuals
+                alert("Loading visuals.");
+                break;
+        }
+    });
+}
+
+$(document).ready(function() {
+    addActiveSwitcher();
+});
